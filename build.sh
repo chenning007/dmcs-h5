a2011981dfac496fc0650e306ea26815a68db451
chenning <chenning@didichuxing.com>
2018-01-29 11:43:44 +0800

    build script

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
 git pull
-npm run build
+npm  run build
+rm -rf /usr/local/nginx/html/dmcs
+mkdir -p /usr/local/nginx/html/dmcs
+echo "copy dist to nginx server ..."
+cp -R dist/* /usr/local/nginx/html/dmcs
+echo "reload nginx.."
+/usr/local/nginx/sbin/nginx -s reload
+echo "succeed!"
