28a0e22b47b7625778fb00c7034bd149bf63d6d4
caizj15 <caizj15@qq.com>
2018-03-21 19:58:04 +0800

    change profile.js

---
+++ src/routes/Profile/Profile.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import { routerRedux, Route, Switch } from 'dva/router';
+import { connect } from 'dva';
+import { Input } from 'antd';
+import PageHeaderLayout from '../../layouts/PageHeaderLayout';
+import { getRoutes } from '../../utils/utils';
+
+@connect()
+export default class Profile extends Component {
+  handleTabChange = (key) => {
+    const { dispatch, match } = this.props;
+    switch (key) {
+      case 'basic-form':
+        dispatch(routerRedux.push(`${match.url}/basic-form`));
+        break;
+      case 'set_up':
+        dispatch(routerRedux.push('${match.url}/set_up'))
+        break;
+      case 'basic':
+        dispatch(routerRedux.push(`${match.url}/basic`));
+        break;
+      case 'advanced':
+        dispatch(routerRedux.push(`${match.url}/advanced`));
+        break;
+      default:
+        break;
+    }
+  }
+
+  render() {
+    const tabList = [{
+      key: 'basic-form',
+      tab: '信息',
+    },
+    {
+        key: 'set_up',
+        tab: '设置',
+      },
+    {
+      key: 'basic',
+      tab: '踪迹',
+    }, 
+    //{
+    //  key: 'applications',
+    //  tab: '应用',
+    //}, 
+    {
+      key: 'advanced',
+      tab: '财产',
+    }];
+
+    const mainSearch = (
+      <div style={{ textAlign: 'center' }}>
+        <Input.Search
+          placeholder="请输入"
+          enterButton="搜索"
+          size="large"
+          onSearch={this.handleFormSubmit}
+          style={{ width: 522 }}
+        />
+      </div>
+    );
+
+    const { match, routerData, location } = this.props;
+    const routes = getRoutes(match.path, routerData);
+
+    return (
+      <PageHeaderLayout
+        title="搜索列表"
+        content={mainSearch}
+        tabList={tabList}
+        activeTabKey={location.pathname.replace(`${match.path}/`, '')}
+        onTabChange={this.handleTabChange}
+      >
+        <Switch>
+          {
+            routes.map(item =>
+              (
+                <Route
+                  key={item.key}
+                  path={item.path}
+                  component={item.component}
+                  exact={item.exact}
+                />
+              )
+            )
+          }
+        </Switch>
+      </PageHeaderLayout>
+    );
+  }
+}
