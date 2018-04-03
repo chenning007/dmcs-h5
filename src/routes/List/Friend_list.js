b50041c81294b4adcfd833f0db0be5a637676baa
caizj15 <caizj15@qq.com>
2018-04-03 22:13:32 +0800

    change Friend_list

--- src/routes/List/Friend_list.js
+++ src/routes/List/Friend_list.js
@@ -2,15 +2,16 @@ import React, { PureComponent } from 'react';
 import moment from 'moment';
 import { connect } from 'dva';
 import { Link } from 'dva/router';
-import { Row, Col, Card, List, Avatar, Input, Button, Icon, Modal, Form } from 'antd';
+import { Row, Col, Card, List, Avatar, Input, Button, Icon, Modal, Form, Popconfirm, Select } from 'antd';
 
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 import EditableLinkGroup from '../../components/EditableLinkGroup';
 import { Radar } from '../../components/Charts';
 
-import styles from './Friend_list.less';
+import styles from './Device_friend.less';
 
 const FormItem = Form.Item;
+const Option=Select.Option;
 
 const links = [
   {
@@ -73,35 +74,22 @@ const members = [
 ];
 
 @connect(state => ({
-  project: state.project,
   activities: state.activities,
-  chart: state.chart, 
 }))
-export default class Friend_list extends PureComponent {
-  state = {
-    addInputValue: '',
-    modalVisible: false,
-    expandForm: false,
-    selectedRows: [],
-    formValues: {},
-  };
+
+@Form.create()
+export default class Device_friend extends PureComponent {
   componentDidMount() {
     const { dispatch } = this.props;
     dispatch({
-      type: 'project/fetchNotice',
-    });
-    dispatch({
       type: 'activities/fetchList',
-    });
-    dispatch({
-      type: 'chart/fetch',
     });
   }
 
   componentWillUnmount() {
     const { dispatch } = this.props;
     dispatch({
-      type: 'chart/clear',
+      type: 'activities/clear',
     });
   }
 /**** */
@@ -120,9 +108,24 @@ export default class Friend_list extends PureComponent {
  /**** */ 
   renderActivities() {
     const {
-      activities: { list },
+      activities: { list_activities },
     } = this.props;
-    return list.map((item) => {
+　　 /*const default_action = (
+      <div>
+        <Select defaultValue="权限设置" style={{ width: 120 }} >
+          <Option value="１">权限1</Option>
+          <Option value="２">权限2</Option>
+          <Option value="３" >权限3</Option>
+          <Option value="４">权限4</Option>
+          <Option value="５">权限5</Option>
+          <Option value="６">权限6</Option>
+          <Option value="７" >权限7</Option>
+          <Option value="makebyself">自定义</Option>
+        </Select>
+      </div>
+    );*/
+
+    return list_activities.map((item) => {
       const events = item.template.split(/@\{([^{}]*)\}/gi).map((key) => {
    //      if (item[key]) {
    //       return <a href={item[key].link} key={item[key].name}>{item[key].name}</a>;
@@ -130,7 +133,12 @@ export default class Friend_list extends PureComponent {
         return key;
      });
       return (
-        <List.Item key={item.id} actions={[<a>删除</a>]}>
+        <List.Item key={item.id} /*actions={[<a>设置</a>, 
+            <Popconfirm title="是否要删除此行？">
+               <a>删除</a>
+            </Popconfirm>]}*/
+            //actions={[default_action]}
+        >
           <List.Item.Meta
             avatar={<Link to="/dashboard/monitor_device">{<Avatar src={item.user.avatar} /> }</Link>}
             title={
@@ -153,34 +161,41 @@ export default class Friend_list extends PureComponent {
 
   render() {
     const {
-      project: { loading: projectLoading, notice },
       activities: { loading: activitiesLoading },
-      chart: { radarData },
+      form,
     } = this.props;
-    const { modalVisible, addInputValue } = this.state;
-    const pageHeaderContent = (
-      <div className={styles.pageHeaderContent}>
-        <div className={styles.avatar}>
-          <Avatar size="large" src="https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png" />
-        </div>
-        <div className={styles.content}>
-          <div className={styles.contentTitle}>早安，蔡志军，祝你开心每一天！</div>
-          {/*<div>交互专家 | 蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED</div>*/}
-        </div>
-      </div>
-    );
+    
+    const { getFieldDecorator, getFieldValue } = this.props.form;
 
-    const extraContent = (
-      <div style={{ textAlign: 'center' }}>
-        <Input.Search
-          placeholder="请输入"
-          enterButton="搜索"
-          size="large"
-          //onSearch={this.handleFormSubmit}
-          style={{ width: 522 }}
-        />
+   /* const extraContent = (
+      <div className={styles.extraContent}>
+      <div className={styles.statItem} >
+         <Icon type="minus" style={{fontSize: 32, color: 'rgb(0, 129, 204)'}}/>
       </div>
-    );
+      <div className={styles.statItem}> 
+         <Icon type="plus" style={{fontSize: 32, color: 'rgb(0, 129, 204)'}} />
+      </div>
+      </div>
+    ); */
+
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24, offset: -10 },
+        sm: { span: 7 , offset: -10 }, 
+      },
+      wrapperCol: {
+        xs: { span: 24, offset: 3},
+        sm: { span: 12 },
+        md: { span: 10 },
+      },
+    };
+
+    const submitFormLayout = {
+      wrapperCol: {
+        xs: { span: 24, offset: 0 },
+        sm: { span: 10, offset: 10 },
+      },
+    };
 
     return (
         <Row gutter={24}>
@@ -191,60 +206,16 @@ export default class Friend_list extends PureComponent {
               className={styles.activeCard}
               title="共享成员"
               loading={activitiesLoading}
+              //extra={extraContent}
+              style={{ marginBottom: 24 }} 
             >
               <List loading={activitiesLoading} size="large">
-                <div className={styles.activitiesList}>
-                 {/*<Link to="/dashboard/analysis">{this.renderActivities()} </Link>*/}
-                       {this.renderActivities()} 
-                </div>
+                   <div className={styles.activitiesList}>
+                      {this.renderActivities()} 
+                   </div>
               </List>
             </Card>
-          </Col>
-          
-      {/*   <Col xl={8} lg={24} md={24} sm={24} xs={24}>
-            <Card
-              style={{ marginBottom: 24 }}
-              title="快速开始 / 便捷导航"
-              bordered={false}
-              bodyStyle={{ padding: 0 }}
-            >
-              <EditableLinkGroup
-                onAdd={() => {}}
-                links={links}
-                linkElement={Link}
-              />
-            </Card>
-            <Card
-              style={{ marginBottom: 24 }}
-              bordered={false}
-              title="XX 指数"
-              loading={radarData.length === 0}
-            >
-              <div className={styles.chart}>
-                <Radar hasLegend height={343} data={radarData} />
-              </div>
-            </Card>
-            <Card
-              bodyStyle={{ paddingTop: 12, paddingBottom: 12 }}
-              bordered={false}
-              title="团队"
-            >
-              <div className={styles.members}>
-                <Row gutter={48}>
-                  {
-                    members.map(item => (
-                      <Col span={12} key={`members-item-${item.id}`}>
-                        <Link to={item.link}>
-                          <Avatar src={item.logo} size="small" />
-                          <span className={styles.member}>{item.title}</span>
-                        </Link>
-                      </Col>
-                    ))
-                  }
-                </Row>
-              </div>
-            </Card>
-                </Col>*/}
+         </Col>
         </Row>
     );
   }
