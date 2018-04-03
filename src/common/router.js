2a3ca8dcde8d84e56dfbbf4b90d7c8790a16e2ed
caizj15 <caizj15@qq.com>
2018-04-03 22:09:06 +0800

    add router.js

--- src/common/router.js
+++ src/common/router.js
@@ -76,7 +76,7 @@ export const getRouterData = (app) => {
       component: dynamicWrapper(app, ['list'], () => import('../routes/List/List')),
     },
     '/list/search/friend_list': {
-      component: dynamicWrapper(app, ['form'], () => import('../routes/Forms/AdvancedForm')),
+      component: dynamicWrapper(app, ['activities'], () => import('../routes/List/Friend_list')),
     },
     '/list/search/device_list': {
       component: dynamicWrapper(app, ['list'], () => import('../routes/List/Device_list')),
