7ca0e90ed9203692bf4b39f75be6c13bbeebd80d
caizj15 <caizj15@qq.com>
2018-04-08 16:19:19 +0800

    change set_up.js

--- src/routes/Profile/Set_up.js
+++ src/routes/Profile/Set_up.js
@@ -1,22 +1,17 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
 import {
-  Input, Select, Button, Card, InputNumber, Icon, Row, Col, Avatar,  List, Divider, Steps } from 'antd';
+  Input, Select, Button, Card, InputNumber, Icon, Row, Col, Avatar,  List, Divider, Steps, message } from 'antd';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 
 import logo from '../../../public/title.png';
 
-//const FormItem = Form.Item;
-//const { Option } = Select;
-//const { RangePicker } = DatePicker;
-//const { TextArea } = Input;
+
 const { Step }=Steps;
 
 @connect(state => ({
   //data: state.form.data,
   //submitting: state.form.regularFormSubmitting,
-  condition: 0,
-  current: 0,
 }))
 //@Form.create()
 export default class Basic_form extends PureComponent {
@@ -31,6 +26,10 @@ export default class Basic_form extends PureComponent {
       }
     });
   }*/
+  state = {
+    condition: 0,
+    current: 0,
+  };
 
   onChangeUserName = (e) => {
     this.setState({ data: e.target.value });
@@ -48,16 +47,27 @@ export default class Basic_form extends PureComponent {
     this.setState({condition: 4});
   }
   next() {
-    const temcurrent=this.state.current+1;
-    this.setState({current: temcurrent});
+    const temcurrent = this.state.current + 1;
+    this.setState({current: temcurrent });
   }
   pre() {
-    const temcurrent=this.state.current-1;
-    this.setState({current: temcurrent});
+    const temcurrent=this.state.current - 1;
+    this.setState({ current: temcurrent });
   }
-
+  componentWillUnmount() {
+    this.setState({current: 0,
+       condition: 0,
+    });
+  }
+  reset() {
+    this.setState({current: 0,
+      condition: 0,
+   });
+   message.success('修改成功');
+  }
+  
   renderSet_up() {
-    const { state }=this.props;
+    //const { state }=this.props;
     const { condition,current }=this.state;
     switch( condition ){
        case 1 : return(
@@ -67,18 +77,18 @@ export default class Basic_form extends PureComponent {
                             <Step title="修改登录密码" />
                             <Step title="完成" />
                          </Steps>
-                         <div className='step-aciton'>
+                         <div style={{ marginTop:20}}>
                            { current<2
                              &&
-                             <Button type='primary' onClick={this.next}>下一步</Button>
+                             <Button type='primary' onClick={() =>this.next()}>下一步</Button>
                            }
-                           { current==2
+                           { current===2
                              &&
-                             <Button type='primary'onClick={()=>message.success('完成修改')}>完成</Button>
+                             <Button type='primary' onClick={()=>this.reset()}>完成</Button>
                            }
                            { current>0
                              &&
-                             <Button type='primary' style={{marginLeft: 8}} onClick={this.pre}>上一步</Button>
+                             <Button type='primary' style={{marginLeft: 8}} onClick={()=>this.pre()}>上一步</Button>
                             }   
                          </div>
                        </Card>
@@ -90,18 +100,18 @@ export default class Basic_form extends PureComponent {
                             <Step title="修改已验证手机" />
                             <Step title="完成" />
                           </Steps>
-                          <div className='step-aciton'>
+                          <div style={{ marginTop:20}}>
                             { current<2
                               &&
-                              <Button type='primary' onClick={this.next}>下一步</Button>
+                              <Button type='primary' onClick={() =>this.next()}>下一步</Button>
                             }
                             { current==2
                               &&
-                              <Button type='primary'onClick={()=>message.success('完成修改')}>完成</Button>
+                              <Button type='primary' onClick={()=>this.reset()}>完成</Button>
                             }
                             { current>0
                               &&
-                              <Button type='primary' style={{marginLeft: 8}} onClick={this.pre}>上一步</Button>
+                              <Button type='primary' style={{marginLeft: 8}} onClick={()=>this.pre()}>上一步</Button>
                             }   
                           </div>
                         </Card>
@@ -116,20 +126,20 @@ export default class Basic_form extends PureComponent {
                           <div className='step-aciton'>
                             { current<2
                               &&
-                              <Button type='primary' onClick={this.next}>下一步</Button>
+                              <Button type='primary' onClick={() =>this.next()}>下一步</Button>
                             }
                             { current==2
                               &&
-                              <Button type='primary'onClick={()=>message.success('完成修改')}>完成</Button>
+                              <Button type='primary' onClick={()=>this.reset()}>完成</Button>
                             }
                             { current>0
                               &&
-                              <Button type='primary' style={{marginLeft: 8}} onClick={this.pre}>上一步</Button>
+                              <Button type='primary' style={{marginLeft: 8}} onClick={()=>this.pre()}>上一步</Button>
                             }   
                           </div>
                         </Card>
                         );
-       case 4 : return ;
+       case 4 : return (this.reset());
 
        default : return (
                       <Row>
@@ -148,7 +158,7 @@ export default class Basic_form extends PureComponent {
                             </List>
                             <Divider type='horizontal'/>
                             <List bordered={false}>
-                              <List.Item actions={[<Button type='dashed' size='large'>修改</Button>]} >
+                              <List.Item actions={[<Button type='dashed' size='large' onClick={this.onChangeState2}>修改</Button>]} >
                                 <Icon type="check-square" style={{fontSize: 48, color:'#7CFC00'}}/>
                                 <span style={{fontSize: 32, paddingLeft: 32}}>手机验证</span>  
                                 <span style={{fontSize: 32}}><Divider type='vertical'/></span>
@@ -159,7 +169,7 @@ export default class Basic_form extends PureComponent {
                             </List>
                             <Divider type='horizontal'/>
                             <List bordered={false}>
-                              <List.Item actions={[<Button type='primary' size='large'>立即绑定</Button>]} >
+                              <List.Item actions={[<Button type='primary' size='large' onClick={this.onChangeState3}>立即绑定</Button>]} >
                               <Icon type="exclamation-circle-o"  style={{fontSize: 48, color:'#FFFF00'}}/>
                                 <span style={{fontSize: 32, paddingLeft: 32}}>邮箱验证</span>  
                                 <span style={{fontSize: 32}}><Divider type='vertical'/></span>
@@ -170,7 +180,7 @@ export default class Basic_form extends PureComponent {
                             </List> 
                             <Divider type='horizontal'/>
                             <List bordered={false}>
-                              <List.Item actions={[<Button type='primary' size='large'>立即认证</Button>]} >
+                              <List.Item actions={[<Button type='primary' size='large' onClick={this.onChangeState4}>立即认证</Button>]} >
                               <Icon type="exclamation-circle-o"  style={{fontSize: 48, color:'#FFFF00'}}/>
                                 <span style={{fontSize: 32, paddingLeft: 32}}>实名认证</span>  
                                 <span style={{fontSize: 32}}><Divider type='vertical'/></span>
@@ -190,7 +200,7 @@ export default class Basic_form extends PureComponent {
   render() {
    // const { submitting, form, data } = this.props;
    // const { getFieldDecorator, getFieldValue } = this.props.form;
-
+    
     const formItemLayout = {
       labelCol: {
         xs: { span: 24 },
@@ -212,57 +222,9 @@ export default class Basic_form extends PureComponent {
     };
 
     return (
-      <Row>
-        <Col span={2}/>
-        <Col span={20} >
-          <Card  /*loading={loading}*/>
-            <List bordered={false}>
-              <List.Item actions={[<Button type='dashed' size='large'>修改</Button>]} >
-                <Icon type="check-square" style={{fontSize: 48, color:'#7CFC00'}}/>
-                <span style={{fontSize: 32, paddingLeft: 32}}>登录密码</span>  
-                <span style={{fontSize: 32}}><Divider type='vertical'/></span>
-                <span style={{paddingTop: 16, color:'#FF0000'}}>
-                      互联网账号存在被盗风险，建议您定期更改密码以保护账户安全
-                </span> 
-              </List.Item>
-            </List>
-            <Divider type='horizontal'/>
-            <List bordered={false}>
-              <List.Item actions={[<Button type='dashed' size='large'>修改</Button>]} >
-                <Icon type="check-square" style={{fontSize: 48, color:'#7CFC00'}}/>
-                <span style={{fontSize: 32, paddingLeft: 32}}>手机验证</span>  
-                <span style={{fontSize: 32}}><Divider type='vertical'/></span>
-                <span style={{paddingTop: 16}}>
-                      你验证的手机:15701585453,若已丢失或停用，<span style={{color:'#FF0000'}}>请立即更换</span>
-                </span> 
-              </List.Item>
-            </List>
-            <Divider type='horizontal'/>
-            <List bordered={false}>
-              <List.Item actions={[<Button type='primary' size='large'>立即绑定</Button>]} >
-              <Icon type="exclamation-circle-o"  style={{fontSize: 48, color:'#FFFF00'}}/>
-                <span style={{fontSize: 32, paddingLeft: 32}}>邮箱验证</span>  
-                <span style={{fontSize: 32}}><Divider type='vertical'/></span>
-                <span style={{paddingTop: 16}}>
-                     验证后，可用于快速找回登录密码,提高账号的稳定性
-                </span> 
-              </List.Item>
-            </List> 
-            <Divider type='horizontal'/>
-            <List bordered={false}>
-              <List.Item actions={[<Button type='primary' size='large'>立即认证</Button>]} >
-              <Icon type="exclamation-circle-o"  style={{fontSize: 48, color:'#FFFF00'}}/>
-                <span style={{fontSize: 32, paddingLeft: 32}}>实名认证</span>  
-                <span style={{fontSize: 32}}><Divider type='vertical'/></span>
-                <span style={{paddingTop: 16}}>
-                     实名认证后，可通过实名信息修改手机号等，提高账户安全性
-                </span> 
-              </List.Item>
-            </List> 
-          </Card>
-        </Col>
-        <Col span={2}/>
-      </Row>
+      <div className='render'>
+      {this.renderSet_up()} 
+      </div>
     );
   }
 }
