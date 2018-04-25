f813b4e9de6b5868bbae81ddb7ba5a803887e961
caizj15 <caizj15@qq.com>
2018-04-25 17:51:22 +0800

    cahnge services/api.js

--- src/services/api.js
+++ src/services/api.js
@@ -4,14 +4,11 @@ import request from '../utils/request';
 export async function queryProjectNotice() {
   return request('/api/project/notice');
 }
-
-export async function queryActivities() {
-  return request('/api/activities');
+////
+export async function queryFriend() {
+  return request('/api/friends');
 }
-export async function query_Activities() {
-  return request('/api/query_activities');
-}
-
+//////
 export async function queryRule(params) {
   return request(`/api/rule?${stringify(params)}`);
 }
