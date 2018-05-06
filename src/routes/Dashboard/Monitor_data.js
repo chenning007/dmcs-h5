6f535c582c4d831d2c7010c98ccfe9afeedabefa
caizj15 <caizj15@qq.com>
2018-05-06 21:54:46 +0800

    change Monitor_datat

--- src/routes/Dashboard/Monitor_data.js
+++ src/routes/Dashboard/Monitor_data.js
@@ -80,7 +80,104 @@ const device_data = [{
     ],
 }
 ];
+ /***** */
+function getActiveData() {
+    const activeData =[];
+    for(let i = 0; i < 15 ; i ++){
+      activeData.push(
+        Math.floor(Math.random()*10)+1
+      );
+    }
+    return activeData;
+}
 
+function getdevice_data() {
+    const activeData = getActiveData();
+    var Device_data = [];
+    if((activeData !==undefined)&&(activeData !==[])&&(activeData.length === 15)){
+      Device_data = [{
+      key: 1,
+      title: '节点１',
+      node: [{
+          key: 1,
+          type: 'S1',
+          data: activeData[1],
+      },{
+          key: 2,
+          type: 'S2',
+          data: activeData[2],  
+      },{
+          key: 3,
+          type: 'S3',
+          data: activeData[3], 
+      },{
+          key: 4,
+          type: 'R1',
+          data: activeData[4], 
+      },{
+          key: 5,
+          type: 'R2',
+          data: activeData[5], 
+      },{
+          key: 6,
+          type: 'R2',
+          data: activeData[6], 
+      }
+      ],
+  },
+  {   key: 2,
+      title: '节点2',
+      node: [{
+          key: 1,
+          type: 'S1',
+          data: activeData[7],
+      },{
+          key: 2,
+          type: 'S2',
+          data: activeData[8],  
+      },{
+          key: 3,
+          type: 'S3',
+          data: activeData[9], 
+      },{
+          key: 4,
+          type: 'R1',
+          data: activeData[10], 
+      },
+      ],
+  },
+  {   key: 3,
+      title: '节点3',
+      node: [{
+          key: 1,
+          type: 'S1',
+          data: activeData[11],
+      },{
+          key: 2,
+          type: 'S2',
+          data: activeData[12],  
+      },{
+          key: 3,
+          type: 'S3',
+          data: activeData[13], 
+      },{
+          key: 4,
+          type: 'R1',
+          data: activeData[14], 
+      },{
+          key: 5,
+          type: 'R2',
+          data: activeData[0], 
+      },
+      ],
+    }
+    ];
+}
+    
+  
+    return Device_data;
+}
+ /***** */
 const columns=[ 
     {title: '端点',width: '40%', dataIndex: 'type', key: 'type'},
     {title: '数据', width: '60%', dataIndex: 'data', key: 'data'}
@@ -89,9 +186,16 @@ const columns=[
 @connect(state => ({ 
 }))
 export default class Monitor_data extends PureComponent {
-  //componentDidMount() {
-  //  this.props.dispatch();
-  //}
+    state = {
+        Device_data: device_data,
+    }
+  componentDidMount() {
+    this.timer = setInterval(() => {
+        this.setState({
+          Device_data: getdevice_data(),
+        });
+      }, 5000);
+  }
 
 
   render() {
@@ -102,7 +206,7 @@ export default class Monitor_data extends PureComponent {
         sm: { span: 16 },
         },
     };
-
+    const {Device_data = []} = this.state;
     return (
       <div>
         <List
@@ -110,7 +214,7 @@ export default class Monitor_data extends PureComponent {
             style={{ marginTop: 24 }}
             grid={{ gutter: 24, xl: 4, lg: 3, md: 3, sm: 2, xs: 1 }}
             //loading={loading}
-            dataSource={device_data}
+            dataSource={Device_data}
             renderItem={item => (
             <List.Item key={item.key}>
                 < Card
