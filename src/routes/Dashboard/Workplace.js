bf930342d832da2b647cd7c22094229548f4bb4c
caizj15 <caizj15@qq.com>
2018-05-06 20:00:23 +0800

    change Workplace.js

--- src/routes/Dashboard/Workplace.js
+++ src/routes/Dashboard/Workplace.js
@@ -132,33 +132,10 @@ export default class Workplace extends PureComponent {
       chart: { radarData },
     } = this.props;
     const { modalVisible, addInputValue } = this.state;
-    const pageHeaderContent = (
-      <div className={styles.pageHeaderContent}>
-        <div className={styles.avatar}>
-          <Avatar size="large" src="https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png" />
-        </div>
-        <div className={styles.content}>
-          <div className={styles.contentTitle}>早安，蔡志军，祝你开心每一天！</div>
-          {/*<div>交互专家 | 蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED</div>*/}
-        </div>
-      </div>
-    );
 
-    const extraContent = (
-      <div style={{ textAlign: 'center' }}>
-        <Input.Search
-          placeholder="请输入"
-          enterButton="搜索"
-          size="large"
-          //onSearch={this.handleFormSubmit}
-          style={{ width: 522 }}
-        />
-      </div>
-    );
 
     return (
-      <PageHeaderLayout
-      >
+      <div>
         <Row gutter={24}>
           <Col xl={24} lg={24} md={24} sm={24} xs={24}>
             <Card
@@ -166,16 +143,9 @@ export default class Workplace extends PureComponent {
               style={{ marginBottom: 24 }}
               title="我的设备"
               bordered={false}
-              //extra={<Link to="/">全部项目</Link>}
               loading={projectLoading}
               bodyStyle={{ padding: 0 }}
             >
-           {/*   <Button type="dashed" style={{ width: '100%', marginBottom: 8 }} icon="plus"
-               onClick={() => this.handleModalVisible(true)}
-              >
-               添加
-              </Button>
-                       */}
               {
                 notice.map(item => (
                   <Card.Grid className={styles.projectGrid} key={item.id}
@@ -212,21 +182,6 @@ export default class Workplace extends PureComponent {
                 ))
               }
             </Card>
-            {/*<Modal
-               title="添加设备"
-               visible={modalVisible}
-               onOk={this.handleAdd}
-               onCancel={() => this.handleModalVisible()}
-            >
-             <FormItem
-                 labelCol={{ span: 5 }}
-                 wrapperCol={{ span: 15 }}
-                 label="描述"
-              >
-                 <Input placeholder="请输入设备编号" onChange={this.handleAddInput} value={addInputValue} />
-              </FormItem>
-            </Modal>
-            */}
            <Card
               bodyStyle={{ padding: 0 }}
               bordered={false}
@@ -272,53 +227,8 @@ export default class Workplace extends PureComponent {
             </Card>
   
           </Col>
-          
-      {/*   <Col xl={8} lg={24} md={24} sm={24} xs={24}>
-            <Card
-              style={{ marginBottom: 24 }}
-              title="快速开始 / 便捷导航"
-              bordered={false}
-              bodyStyle={{ padding: 0 }}
-            >
-              <EditableLinkGroup
-                onAdd={() => {}}
-                links={links}
-                linkElement={Link}
-              />
-            </Card>
-            <Card
-              style={{ marginBottom: 24 }}
-              bordered={false}
-              title="XX 指数"
-              loading={radarData.length === 0}
-            >
-              <div className={styles.chart}>
-                <Radar hasLegend height={343} data={radarData} />
-              </div>
-            </Card>
-            <Card
-              bodyStyle={{ paddingTop: 12, paddingBottom: 12 }}
-              bordered={false}
-              title="团队"
-            >
-              <div className={styles.members}>
-                <Row gutter={48}>
-                  {
-                    members.map(item => (
-                      <Col span={12} key={`members-item-${item.id}`}>
-                        <Link to={item.link}>
-                          <Avatar src={item.logo} size="small" />
-                          <span className={styles.member}>{item.title}</span>
-                        </Link>
-                      </Col>
-                    ))
-                  }
-                </Row>
-              </div>
-            </Card>
-                </Col>*/}
         </Row>
-      </PageHeaderLayout>
+      </div>
     );
   }
 }
