bae6d142030903e31ca3804c54003b3abf2ce5cb
caizj15 <caizj15@qq.com>
2018-05-13 01:35:06 +0800

    change mock/device.js

--- mock/device.js
+++ mock/device.js
@@ -8,6 +8,8 @@ const titles = [
   '微波炉',
   '灯泡',
   '开关',
+  '压力表',
+  '测温计',
 ];
 const avatars = [
   'https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png', // Alipay
@@ -18,6 +20,21 @@ const avatars = [
   'https://gw.alipayobjects.com/zos/rmsportal/kZzEzemZyKLKFsojXItE.png', // React
   'https://gw.alipayobjects.com/zos/rmsportal/ComBAopevLwENQdKWiIn.png', // Vue
   'https://gw.alipayobjects.com/zos/rmsportal/nxkuOJlFJuAUhzlMTCEe.png', // Webpack
+];
+const avatars1 = [
+  'file:///usr/mygit/dmcs-h5/public/image/device1.jpeg',
+  'file:///usr/mygit/dmcs-h5/public/image/device2.jpeg',
+  'file:///usr/mygit/dmcs-h5/public/image/device3.jpeg',
+  'file:///usr/mygit/dmcs-h5/public/image/device4.jpeg',
+  'file:///usr/mygit/dmcs-h5/public/image/device5.jpeg',
+  'file:///usr/mygit/dmcs-h5/public/image/device6.jpeg',
+  'file:///usr/mygit/dmcs-h5/public/image/device7.jpeg',
+  'file:///usr/mygit/dmcs-h5/public/image/device8.jpeg',
+  'file:///usr/mygit/dmcs-h5/public/image/device9.jpeg',
+  'file:///usr/mygit/dmcs-h5/public/image/device10.jpeg',
+  'file:///usr/mygit/dmcs-h5/public/image/device11.jpeg',
+  'file:///usr/mygit/dmcs-h5/public/image/device12.jpeg',
+  'file:///usr/mygit/dmcs-h5/public/image/device13.jpeg',
 ];
 const deviceNumber = [
   2018010000001,
@@ -30,6 +47,23 @@ const deviceNumber = [
   2018010000008,
   2018010000009,
 ];
+const will_number = [
+  2017010000001,
+  2017010000002,
+  2017010000003,
+  2017010000004,
+  2017010000005,
+  2017010000006,
+  2017010000007,
+  2017010000008,
+  2017010000009,
+  2017010000010,
+  2017010000011,
+  2017010000012,
+  2017010000013,
+  2017010000014,
+  2017010000015, //15
+]
 const description = [
   '观测天下',
   '永远吃最新鲜的水果',
@@ -49,10 +83,29 @@ const time = [
   '2016-06-06',
   '2017-07-07',
   '2018-08-08',
+  '2016-05-01',
+  '2016-06-01',
+  '2016-07-01',
+  '2016-08-01', //12
+  '2016-09-01',
+  '2016-10-01',
+  '2016-11-01',//15
+];
+const secret = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'k',
+];
+//但是这里没有显示出来设备的具体的信息
 
-] 
-
-export function fakeList(count) {
+export function deviceList(count) {
   const list = [];
   for (let i = 0; i < count; i += 1) {
     list.push({
@@ -62,6 +115,19 @@ export function fakeList(count) {
       avatar: avatars[i % 8],
       description: description[i % 8],
       time: time[i % 8],
+    });
+  }
+  return list;
+}
+export function will_deviceList(count) {
+  const list = [];
+  for (let i = 0; i < count; i += 1) {
+    list.push({
+      key: i,
+      deviceNumber: deviceNumber[i%  10],
+      defaulttitle: titles[i % 10],
+      defaultavatar: avatars1[i % 10],
+      secret: secret[i %10],
     });
   }
   return list;
@@ -76,8 +142,9 @@ export function getDeviceList(req, res, u) {
   const count = 8;
 
   const result = {
-      myself_device: fakeList(count),
+      myself_device: deviceList(count),
       public_device: [],
+      device_will: will_deviceList(10),
       loading: true,
     };
   if (res && res.json) {
