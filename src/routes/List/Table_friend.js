377f4cb776d06f31a0f7e26bd4afef27ed5a4215
caizj15 <caizj15@qq.com>
2018-05-16 00:38:52 +0800

    change Table_friend.js

--- src/routes/List/Table_friend.js
+++ src/routes/List/Table_friend.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import { Table, Button, Input, message, Popconfirm, Divider, Switch, Icon, Avatar } from 'antd';
 
 
@@ -19,104 +19,9 @@ const avatar = [
 
 /****** */
 /***** **/
-
-const columns = [
-    { title: '用户', width: 60, dataIndex: 'avatar', key: 'avatar', fixed: 'left',
-      render: text =>  <Avatar src={text}/>,
-    },
-    { title: '', width: 100, dataIndex: 'userName', key: 'userName', fixed: 'left'  },
-    { title: '权限1', dataIndex: 'auth1', key:'1', width: 150, 
-      render: (auth1) => <span style={{paddingLeft: 8}}>
-                            <Switch checkedChildren={<Icon type="check" />} 
-                                unCheckedChildren={<Icon type="cross" />} 
-                                defaultChecked={auth1}
-                            />
-                          </span>,
-    },
-    { title: '权限2', dataIndex: 'auth2', key: '2', width: 150, 
-      render: (auth2) =>  <span style={{paddingLeft: 8}}>
-                                <Switch checkedChildren={<Icon type="check" />} 
-                                    unCheckedChildren={<Icon type="cross" />} 
-                                    defaultChecked={auth2}
-                                />
-                            </span>,
-        
-    },
-    { title: '权限 3', dataIndex: 'auth3', key: '3', width: 150,
-      render: (auth3) =>  <span style={{paddingLeft: 8}}>
-                                <Switch checkedChildren={<Icon type="check" />} 
-                                    unCheckedChildren={<Icon type="cross" />} 
-                                    defaultChecked={auth3}
-                                />
-                            </span>
-        
-    },
-    { title: '权限 4', dataIndex: 'auth4', key: '4', width: 150, 
-        render: (auth4) =>  <span style={{paddingLeft: 8}}>
-                            <Switch checkedChildren={<Icon type="check" />} 
-                                unCheckedChildren={<Icon type="cross" />} 
-                                defaultChecked={auth4}
-                            />
-                            </span>
-        
-    },
-    { title: '权限 5', dataIndex: 'auth5', key: '5', width: 150, 
-        render: (auth5) =>  <span style={{paddingLeft: 8}}>
-                            <Switch checkedChildren={<Icon type="check" />} 
-                                unCheckedChildren={<Icon type="cross" />} 
-                                defaultChecked={auth5}
-                            />
-                            </span>
-        
-    },
-    { title: '权限 6', dataIndex: 'auth6', key: '6', width: 150,
-        render: (auth6) =>  <span style={{paddingLeft: 8}}>
-                            <Switch checkedChildren={<Icon type="check" />} 
-                                unCheckedChildren={<Icon type="cross" />} 
-                                defaultChecked={auth6}
-                            />
-                            </span>
-        
-    },
-    { title: '权限 7', dataIndex: 'auth7', key: '7', width: 150,
-        render: (auth7) =>  <span style={{paddingLeft: 8}}>
-                            <Switch checkedChildren={<Icon type="check" />} 
-                                unCheckedChildren={<Icon type="cross" />} 
-                                defaultChecked={auth7}
-                            />
-                            </span>
-         
-    },
-    { title: '权限 8', dataIndex: 'auth8', key: '8', width: 150,
-        render: (auth8) =>  <span style={{paddingLeft: 8}}>
-                            <Switch checkedChildren={<Icon type="check" />} 
-                                unCheckedChildren={<Icon type="cross" />} 
-                                defaultChecked={auth8}
-                            />
-                            </span>
-         
-    },
-    {title: '权限9', dataIndex: 'auth9', key: '9', width: 150, 
-        render: (auth9) =>  <span style={{paddingLeft: 8}}>
-                            <Switch checkedChildren={<Icon type="check" />} 
-                                unCheckedChildren={<Icon type="cross" />} 
-                                defaultChecked={auth9}
-                            />
-                            </span>
-         
-    },
-    {
-      title: 'Action',
-      key: 'operation',
-      fixed: 'right',
-      width: 100,
-      render: () => <a>action</a>,
-    },
-  ];
-  
-  const data = [];
+  const Data1 = [];
   for (let i = 0; i < 100; i++) {
-    data.push({
+    Data1.push({
       key: i,
       avatar: avatar[i % 10],
       userName: `Edrward ${i}`,
@@ -137,6 +42,8 @@ const columns = [
 
 /****** */
 
+
+
 /****** */
 
 export default class Table_friend extends PureComponent{
@@ -148,10 +55,244 @@ export default class Table_friend extends PureComponent{
             loading: false,
         }
     }
+
+    getRowByKey(key, newData) {
+        return (newData || this.state.data).filter(item => item.key === key)[0];
+    }
+
+    cacheOriginData = {};
+    toggleEditable=(e, key) => {
+        e.preventDefault();
+        const newData = this.state.data.map(item => ({ ...item }));
+        const target = this.getRowByKey(key, newData);
+        if (target) {
+          // 进入编辑状态时保存原始数据
+          if (!target.editable) {
+            this.cacheOriginData[key] = { ...target };
+          }
+          target.editable = !target.editable;
+          this.setState({ data: newData });
+        }
+      }
+    
+    //
+    edit(e, key) {
+        let edit_enable = true;
+        this.toggleEditable(e,key);
+        //this.props.callbackParent(edit_enable);
+    }
+    //这里是向父组件传值，为啥不可行;
+    saveRow(e, key) {
+        e.persist();
+        this.setState({
+          loading: true,
+        });
+        setTimeout(() => {
+          if (this.clickedCancel) {
+            this.clickedCancel = false;
+            return;
+          }
+          const target = this.getRowByKey(key) || {};
+          /*if (!target.workId || !target.name || !target.department) {
+            message.error('请填写完整成员信息。');
+            e.target.focus();
+            this.setState({
+              loading: false,
+            });
+            return;
+          }*/
+          delete target.isNew;
+          this.toggleEditable(e, key);
+          let edit_enable = true;
+          this.props.onChange(this.state.data, edit_enable);
+          
+          this.setState({
+            loading: false,
+          });
+        }, 500);
+      }
+    cancel(e, key) {
+        this.clickedCancel = true;
+        e.preventDefault();
+        const newData = this.state.data.map(item => ({ ...item }));
+        const target = this.getRowByKey(key, newData);
+        if (this.cacheOriginData[key]) {
+            Object.assign(target, this.cacheOriginData[key]);
+            target.editable = false;
+            delete this.cacheOriginData[key];
+        }
+        this.setState({ data: newData });
+        this.clickedCancel = false;
+    }
+//这里已经对newData进行了直接的操作    
+    handleFieldChange(e, fieldName, key) {
+        const newData = this.state.data.map(item => ({ ...item }));
+        const target = this.getRowByKey(key, newData);
+        if (target) {
+            target[fieldName] = e;
+            this.setState({ data: newData });
+        }
+    }
+
     render() {
-        const { list_device_friend=[] } = this.state;
+
+        const columns = [
+            { title: '朋友', width: 60, dataIndex: 'avatar', key: 'avatar', fixed: 'left',
+              render: text =>  <Avatar src={text}/>,
+            },
+            { title: '', width: 100, dataIndex: 'userTitle', key: 'userTitle', fixed: 'left'  },
+            { title: '权限1', dataIndex: 'auth1', key:'1', width: 150, 
+              render: (auth1,record) =>
+              <span style={{paddingLeft: 8}}>
+                    <Switch 
+                        checkedChildren={<Icon type='check'/>}
+                        defaultChecked={auth1}
+                        unCheckedChildren={<Icon type="cross" />}
+                        disabled={record.editable? false:true}
+                        onChange={e => this.handleFieldChange(e, 'auth1', record.key)}
+                    />
+                </span>
+            },
+            { title: '权限2', dataIndex: 'auth2', key: '2', width: 150, 
+              render: (auth2,record) =>  
+              <span style={{paddingLeft: 8}}>
+                    <Switch 
+                        checkedChildren={<Icon type='check'/>}
+                        defaultChecked={auth2}
+                        unCheckedChildren={<Icon type="cross" />}
+                        disabled={record.editable? false:true}
+                        onChange={e => this.handleFieldChange(e, 'auth2', record.key)}
+                    />
+                </span>
+                
+            },
+            { title: '权限 3', dataIndex: 'auth3', key: '3', width: 150,
+              render: (auth3, record) =>  
+                <span style={{paddingLeft: 8}}>
+                    <Switch 
+                        checkedChildren={<Icon type='check'/>}
+                        defaultChecked={auth3}
+                        unCheckedChildren={<Icon type="cross" />}
+                        disabled={record.editable? false:true}
+                        onChange={e => this.handleFieldChange(e, 'auth3', record.key)}
+                    />
+                </span>
+                
+            },
+            { title: '权限 4', dataIndex: 'auth4', key: '4', width: 150, 
+                render: (auth4,record) =>  
+                <span style={{paddingLeft: 8}}>
+                    <Switch 
+                        checkedChildren={<Icon type='check'/>}
+                        defaultChecked={auth4}
+                        unCheckedChildren={<Icon type="cross" />}
+                        disabled={record.editable? false:true}
+                        onChange={e => this.handleFieldChange(e, 'auth4', record.key)}
+                    />
+                </span>
+                
+            },
+            { title: '权限 5', dataIndex: 'auth5', key: '5', width: 150, 
+                render: (auth5,record) =>  
+                <span style={{paddingLeft: 8}}>
+                    <Switch 
+                        checkedChildren={<Icon type='check'/>}
+                        defaultChecked={auth5}
+                        unCheckedChildren={<Icon type="cross" />}
+                        disabled={record.editable? false:true}
+                        onChange={e => this.handleFieldChange(e, 'auth5', record.key)}
+                    />
+                </span>
+                
+            },
+            { title: '权限 6', dataIndex: 'auth6', key: '6', width: 150,
+                render: (auth6,record) =>  
+                <span style={{paddingLeft: 8}}>
+                    <Switch 
+                        checkedChildren={<Icon type='check'/>}
+                        defaultChecked={auth6}
+                        unCheckedChildren={<Icon type="cross" />}
+                        disabled={record.editable? false:true}
+                        onChange={e => this.handleFieldChange(e, 'auth6', record.key)}
+                    />
+                </span>
+                
+            },
+            { title: '权限 7', dataIndex: 'auth7', key: '7', width: 150,
+                render: (auth7, record) =>  
+                <span style={{paddingLeft: 8}}>
+                    <Switch 
+                        checkedChildren={<Icon type='check'/>}
+                        defaultChecked={auth7}
+                        unCheckedChildren={<Icon type="cross" />}
+                        disabled={record.editable? false:true}
+                        onChange={e => this.handleFieldChange(e, 'auth7', record.key)}
+                    />
+                </span>
+                 
+            },
+            { title: '权限 8', dataIndex: 'auth8', key: '8', width: 150,
+              render: (auth8, record) =>  
+                <span style={{paddingLeft: 8}}>
+                    <Switch 
+                        checkedChildren={<Icon type='check'/>}
+                        defaultChecked={auth8}
+                        unCheckedChildren={<Icon type="cross" />}
+                        disabled={record.editable? false:true}
+                        onChange={e => this.handleFieldChange(e, 'auth8', record.key)}
+                    />
+                </span>       
+            },
+            { title: '权限9', dataIndex: 'auth9', key: '9', width: 150, 
+              render: (auth9, record) =>  
+                <span style={{paddingLeft: 8}}>
+                    <Switch 
+                        checkedChildren={<Icon type='check'/>}
+                        defaultChecked={auth9}
+                        unCheckedChildren={<Icon type="cross" />}
+                        disabled={record.editable? false:true}
+                        onChange={e => this.handleFieldChange(e, 'auth9', record.key)}
+                    />
+                </span>      
+            },
+            {
+              title: '设置',
+              key: 'operation',
+              fixed: 'right',
+              width: 150,
+              render: (text ,record) => {
+                if(!!record.editable && this.state.loading) {
+                  return null;
+                }
+                if(record.editable) {
+                  return (
+                    <span>
+                      <a onClick={e => this.saveRow(e, record.key)}>保存</a>
+                      <Divider type='vertical'/>
+                      <a onClick={e => this.cancel(e,record.key)}>取消</a>
+                    </span>
+                  )
+                }
+                return (
+                  <span>
+                    <a onClick={e => this.edit(e, record.key)}>
+                      编辑
+                    </a>
+                  </span>
+                );
+              }
+            },
+          ];
+        const { data = [] } = this.state;
         return (
-            <Table columns={columns} dataSource={data} scroll={{ x:1500, y:300}}/>
+            <Fragment>
+               <Table 
+                loading={this.state.loading}
+                columns={columns} 
+                dataSource={data} 
+                scroll={{ x:1660, y:500}}
+               />
+            </Fragment>
         );
     }   
 
