9388c78f1cf9d9a93faa19dbec27511cd2e50000
caizj15 <caizj15@qq.com>
2018-05-20 01:36:02 +0800

    change src/index.js

--- src/index.js
+++ src/index.js
@@ -5,17 +5,22 @@ import FastClick from 'fastclick';
 import './g2';
 import './rollbar';
 import onError from './error';
+import createHistory from 'history/createHashHistory';
 // import browserHistory from 'history/createBrowserHistory';
+//import createLoading from 'dva-loading';
 import './index.less';
 // 1. Initialize
 const app = dva({
+  history: createHistory(),
+});
+/*const app = dva({
   // history: browserHistory(),
   onError,
-});
+});*/
 
 // 2. Plugins
 
-// app.use({});
+//`app.use(createLoading());
 
 // 3. Register global model,这个不知道有没有调用过
 app.model(require('./models/global'));
@@ -27,4 +32,5 @@ app.router(require('./router'));
 
 // 5. Start
 app.start('#root');
-FastClick.attach(document.body);
+
+export default app._store;
