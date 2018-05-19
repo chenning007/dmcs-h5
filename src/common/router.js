092e152618d99c6bc2e43e6211de69df67d0d51f
caizj15 <caizj15@qq.com>
2018-05-20 01:34:45 +0800

    change common/router.js

--- src/common/router.js
+++ src/common/router.js
@@ -1,21 +1,61 @@
-import React from 'react';
+import { createElement } from 'react';
 import dynamic from 'dva/dynamic';
+import pathToRegexp from 'path-to-regexp';
 import { getMenuData } from './menu';
 
-// wrapper of dynamic
-const dynamicWrapper = (app, models, component) => dynamic({
-  app,
-  // eslint-disable-next-line no-underscore-dangle
-  models: () => models.filter(m => !app._models.some(({ namespace }) => namespace === m)).map(m => import(`../models/${m}.js`)),
-  //  add routerData prop
-  component: () => {
-    const routerData = getRouterData(app);
-    return component().then((raw) => {
-      const Component = raw.default || raw;
-      return props => <Component {...props} routerData={routerData} />;
+let routerDataCache;
+
+//
+const modelNotExisted = (app, model) => (
+  // eslint-disable-next-line
+  !app._models.some(({ namespace }) => {
+    return namespace === model.substring(model.lastIndexOf('/') + 1);
+  })
+);
+
+const dynamicWrapper = (app, models, component) => {
+  // () => require('module')
+  // transformed by babel-plugin-dynamic-import-node-sync
+  if (component.toString().indexOf('.then(') < 0) {
+    models.forEach((model) => {
+      if (modelNotExisted(app, model)) {
+        // eslint-disable-next-line
+        app.model(require(`../models/${model}`).default);
+      }
     });
-  },
-});
+    return (props) => {
+      if (!routerDataCache) {
+        routerDataCache = getRouterData(app);
+      }
+      return createElement(component().default, {
+        ...props,
+        routerData: routerDataCache,
+      });
+    };
+  }
+  // () => import('module')
+  return dynamic({
+    app,
+    models: () => models.filter(
+      model => modelNotExisted(app, model)).map(m => import(`../models/${m}.js`)
+    ),
+    // add routerData prop
+    component: () => {
+      if (!routerDataCache) {
+        routerDataCache = getRouterData(app);
+      }
+      return component().then((raw) => {
+        const Component = raw.default || raw;
+        return props => createElement(Component, {
+          ...props,
+          routerData: routerDataCache,
+        });
+      });
+    },
+  });
+};
+// wrapper of dynamic
+
 
 function getFlatMenuData(menus) {
   let keys = {};
@@ -31,7 +71,7 @@ function getFlatMenuData(menus) {
 }
 
 export const getRouterData = (app) => {
-  const routerData = {
+  const routerConfig = {
     '/': {
       component: dynamicWrapper(app, ['user', 'login'], () => import('../layouts/BasicLayout')),
     },
@@ -153,12 +193,26 @@ export const getRouterData = (app) => {
   };
   // Get name from ./menu.js or just set it in the router data.
   const menuData = getFlatMenuData(getMenuData());
-  const routerDataWithName = {};
-  Object.keys(routerData).forEach((item) => {
-    routerDataWithName[item] = {
-      ...routerData[item],
-      name: routerData[item].name || menuData[item.replace(/^\//, '')],
+
+  const routerData = {};
+
+  Object.keys(routerConfig).forEach((path) => {
+
+    const pathRegexp = pathToRegexp(path);
+    const menuKey = Object.keys(menuData).find(key => pathRegexp.test(`${key}`));
+    let menuItem = {};
+    
+    if (menuKey) {
+      menuItem = menuData[menuKey]; 
+    }
+    let router = routerConfig[path];
+    
+    router = {
+      ...router,
+      name: router.name || menuItem.name,
+      authority: router.authority || menuItem.authority,
     };
+    routerData[path] = router;
   });
-  return routerDataWithName;
+  return routerData;
 };
