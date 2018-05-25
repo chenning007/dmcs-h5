1186dafe62a38fa579963d82cf30ce67725d979b
caizj15 <caizj15@qq.com>
2018-05-25 18:04:44 +0800

    change RegisterResult

--- src/routes/User/RegisterResult.js
+++ src/routes/User/RegisterResult.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Button } from 'antd';
 import { Link } from 'dva/router';
 import Result from '../../components/Result';
@@ -13,13 +13,22 @@ const actions = (
   </div>
 );
 
-export default () => (
-  <Result
-    className={styles.registerResult}
-    type="success"
-    title={title}
-    description="激活邮件已发送到你的邮箱中，邮件有效期为24小时。请及时登录邮箱，点击邮件中的链接激活帐户。"
-    actions={actions}
-    style={{ marginTop: 56 }}
-  />
-);
+
+export default class RegisterResult extends Component { 
+  show_title() {
+    if(this.props.location.state!== undefined){
+      return (<div className={styles.title}>你的账户：{this.props.location.state.email} 注册成功</div>);
+    }
+    return title;
+  }
+  render() { 
+    return ( <Result
+      className={styles.registerResult}
+      type="success"
+      title={this.show_title()}
+      description="激活邮件已发送到你的邮箱中，邮件有效期为24小时。请及时登录邮箱，点击邮件中的链接激活帐户。"
+      actions={actions}
+      style={{ marginTop: 56 }}
+    />);
+  }
+};
