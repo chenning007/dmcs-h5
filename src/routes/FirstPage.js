2ca5b5cc07029ace44421df581f7c44cbd53ca35
caizj15 <caizj15@qq.com>
2018-06-06 14:27:14 +0800

    change Login.js

--- src/routes/FirstPage.js
+++ src/routes/FirstPage.js
@@ -1,7 +1,8 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
-import { Row, Col, Card, Table, Icon, Divider } from 'antd';
-
+import { Row, Col, Card, Table, Icon, Divider, Menu, Dropdown,Button,message } from 'antd';
+import { Layout } from 'antd';
+const { Header, Footer, Sider, Content} = Layout ;
 const columns = [{
   title: 'Name',
   dataIndex: 'name',
@@ -30,17 +31,95 @@ const columns = [{
   ),
 }];
 
-class FirstPage extends PureComponent {
-  componentDidMount() {
+export default class FirstPage extends PureComponent {
+  /*componentDidMount() {
     this.props.dispatch({
       type: 'user/fetch',
     });
+  }*/
+  
+  handleMenuClick(e){
+    return;
   }
-  render() {
-    const { user: { list, loading } } = this.props;
+
+  menu = (
+    <Menu onClick={this.handleMenuClick}>
+      <Menu.Item key="1">1st menu item</Menu.Item>
+      <Menu.Item key="2">2nd menu item</Menu.Item>
+      <Menu.Item key="3">3rd item</Menu.Item>
+    </Menu>
+  );
+
+  showheader() {
     return (
       <div>
-        <Row gutter={24}>
+        <Button type='primary'>登录</Button>
+        <Divider type='vertical'/>
+        <Button type='primary'>注册</Button>
+      </div>
+    );
+  }
+
+  showheader_select() {
+    return(
+      <Row>
+        <Col style={{width:'20%'}}>
+          <Dropdown overlay={this.menu}>
+            <Button style={{ marginLeft: 8 }}>
+              Button <Icon type="down" />
+            </Button>
+          </Dropdown>
+        </Col>
+        <Col style={{width:'20%'}}>
+          <Dropdown overlay={this.menu}>
+            <Button style={{ marginLeft: 8 }}>
+              Button <Icon type="down" />
+            </Button>
+          </Dropdown>
+        </Col>
+        <Col style={{width:'20%'}}>
+          <Dropdown overlay={this.menu}>
+            <Button style={{ marginLeft: 8 }}>
+              Button <Icon type="down" />
+            </Button>
+          </Dropdown>
+        </Col>
+        <Col style={{width:'20%'}}>
+          <Dropdown overlay={this.menu}>
+            <Button style={{ marginLeft: 8 }}>
+              Button <Icon type="down" />
+            </Button>
+          </Dropdown>
+        </Col>
+        <Col style={{width:'20%'}}>
+          <Dropdown overlay={this.menu}>
+            <Button style={{ marginLeft: 8 }}>
+              Button <Icon type="down" />
+            </Button>
+          </Dropdown>
+        </Col>
+      </Row>
+    )
+  }
+  render() {
+    //const { user: { list, loading } } = this.props;
+    return (
+      <div>
+        <Layout>
+          <Header style={{background:'#f0f2f5'}}>
+            {this.showheader()}
+          </Header>
+          <Header style={{color:'#FFFAFA'}}>
+            {this.showheader_select()}
+          </Header>
+          <Content>
+            content
+          </Content>
+          <Footer>
+            Footer
+          </Footer>
+        </Layout>
+        {/*<Row gutter={24}>
           <Col span={8}>
             <Card bordered={false}>
               <p>卡片内容</p>
@@ -86,15 +165,16 @@ class FirstPage extends PureComponent {
               bordered={false}
               extra={<Icon type="setting" />}
             >
-              <Table dataSource={list} loading={loading} columns={columns} />
-            </Card>
+            {/*  <Table dataSource={list} loading={loading} columns={columns} />*/}
+          {/*}  </Card>
           </Col>
-        </Row>
+        </Row>*/}
+
       </div>
     );
   }
 }
 
-export default connect(state => ({
+/*export default connect(state => ({
   user: state.user,
-}))(Dashboard);
+}))(Dashboard);*/
