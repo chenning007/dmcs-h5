ccfd0567c025cd9683c80d81b7f5aff90a564ad9
caizj15 <caizj15@qq.com>
2018-06-06 14:30:41 +0800

    change Registerresult -n

--- src/routes/User/RegisterResult.js
+++ src/routes/User/RegisterResult.js
@@ -3,6 +3,7 @@ import { Button } from 'antd';
 import { Link } from 'dva/router';
 import Result from '../../components/Result';
 import styles from './RegisterResult.less';
+import logo from '../../assets/logo.svg';
 
 const title = <div className={styles.title}>你的账户：AntDesign@example.com 注册成功</div>;
 
@@ -22,13 +23,25 @@ export default class RegisterResult extends Component {
     return title;
   }
   render() { 
-    return ( <Result
-      className={styles.registerResult}
-      type="success"
-      title={this.show_title()}
-      description="激活邮件已发送到你的邮箱中，邮件有效期为24小时。请及时登录邮箱，点击邮件中的链接激活帐户。"
-      actions={actions}
-      style={{ marginTop: 56 }}
-    />);
+    return (
+    <div style={{paddingTop:110 ,paddingRight:0 ,paddingBottom:144 ,paddingLeft:0}}>
+      <div className={styles.top}>
+        <div className={styles.header}>
+          <Link to="/">
+            <img alt="logo" className={styles.logo} src={logo} />
+            <span className={styles.title}>DMCS Valley</span>
+          </Link>
+        </div>
+        <div className={styles.desc}>DMCS Valley 最先进的物联网解决方案专家</div>
+      </div>
+      <Result
+        className={styles.registerResult}
+        type="success"
+        title={this.show_title()}
+        description="激活邮件已发送到你的邮箱中，邮件有效期为24小时。请及时登录邮箱，点击邮件中的链接激活帐户。"
+        actions={actions}
+        style={{ marginTop: 56 }}
+      />
+    </div>);
   }
 };
