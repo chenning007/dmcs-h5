7d6a08cc3ea0db026e820aa3b5a81efdad6426fb
caizj15 <caizj15@qq.com>
2018-09-04 09:38:02 +0800

    add .webpackrc.js

---
+++ .webpackrc.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+export default {
+  entry: 'src/index.js',
+  extraBabelPlugins: [['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]],
+  env: {
+    development: {
+      extraBabelPlugins: ['dva-hmr'],
+    },
+  },
+  externals: {
+    '@antv/data-set': 'DataSet',
+    rollbar: 'rollbar',
+  },
+  //
+  proxy:{
+    "/api/v1/user/image":{
+      "target":"http://127.0.0.1:8080/dmcs",
+      "changeOrigin":true,
+      "pathRewrite":{"^api/v1/user/image":""}
+    }
+  },
+  //
+  alias: {
+    components: path.resolve(__dirname, 'src/components/'),
+  },
+  ignoreMomentLocale: true,
+  theme: './src/theme.js',
+  html: {
+    template: './src/index.ejs',
+  },
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+  },
+  disableDynamicImport: true,
+  publicPath: '/',
+  hash: true,
+};
