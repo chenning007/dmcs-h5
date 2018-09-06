f9e6b00707e6f458cb120306909afab4da633b3a
caizj15 <caizj15@qq.com>
2018-09-06 09:10:06 +0800

    copy src/index from dmcs-project

--- src/index.js
+++ src/index.js
@@ -1,36 +1,29 @@
-import 'babel-polyfill';
+import './polyfill';
 import dva from 'dva';
-import 'moment/locale/zh-cn';
-import FastClick from 'fastclick';
-import './g2';
-import './rollbar';
-import onError from './error';
+
 import createHistory from 'history/createHashHistory';
-// import browserHistory from 'history/createBrowserHistory';
-//import createLoading from 'dva-loading';
+// user BrowserHistory
+// import createHistory from 'history/createBrowserHistory';
+import createLoading from 'dva-loading';
+import 'moment/locale/zh-cn';
+import './rollbar';
+
 import './index.less';
 // 1. Initialize
 const app = dva({
   history: createHistory(),
 });
-/*const app = dva({
-  // history: browserHistory(),
-  onError,
-});*/
 
 // 2. Plugins
+app.use(createLoading());
 
-//`app.use(createLoading());
-
-// 3. Register global model,这个不知道有没有调用过
-app.model(require('./models/global'));
+// 3. Register global model
+app.model(require('./models/global').default);
 
 // 4. Router
-app.router(require('./router'));
-
-
+app.router(require('./router').default);
 
 // 5. Start
 app.start('#root');
 
-export default app._store;
+export default app._store; // eslint-disable-line
