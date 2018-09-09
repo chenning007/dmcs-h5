bd1b01b630df4a1f7e7a806f8a696581c74a3df3
caizj15 <caizj15@qq.com>
2018-09-09 23:06:33 +0800

    change document

--- src/models/document.js
+++ src/models/document.js
@@ -1,63 +1,63 @@
-import {getdocument, deletedocument} from '../services/api';
-export default{
-    namespace:'document',
-    state: {
-        tech_document:[],
-        loading:false,
+import { getdocument, deletedocument } from '../services/api';
+export default {
+  namespace: 'document',
+  state: {
+    tech_document: [],
+    loading: false,
+  },
+
+  effects: {
+    *getDocument(payload, { call, put }) {
+      yield put({
+        type: 'changeLoading',
+        payload: true,
+      });
+      const response = yield call(getdocument, payload);
+      yield put({
+        type: 'changeTech_document',
+        payload: response,
+      });
+      yield put({
+        type: 'changeLoading',
+        payload: false,
+      });
     },
 
-    effects: {
-        *getDocument(_,{call,put}) {
-            yield put({
-                type:'changeLoading',
-                payload: true,
-            })
-            const response = yield call(getdocument);
-            yield put({
-                type:'changeTech_document',
-                payload: response,
-            })
-            yield put({
-                type:'changeLoading',
-                payload: false,
-            })
-        },
-
-        *deleteDocument({payload},{call,put}) {
-            yield put({
-                type:'changeLoading',
-                payload: true,
-            })
-            const response =yield call(deletedocument, payload)
-            yield put({
-                type:'deleTech_document',
-                payload: response,
-            })
-            yield put({
-                type:'changeLoading',
-                payload: true,
-            })
-        }
+    *deleteDocument({ payload }, { call, put }) {
+      yield put({
+        type: 'changeLoading',
+        payload: true,
+      });
+      const response = yield call(deletedocument, payload);
+      yield put({
+        type: 'deleTech_document',
+        payload: response,
+      });
+      yield put({
+        type: 'changeLoading',
+        payload: true,
+      });
     },
+  },
 
-    reducers :{
-        clear() {
-            return{
-                loading:fasle,
-                tech_document: [],
-            }
-        },
-        changeTech_document(state, {payload}) {
-            return{
-                ...state,
-                tech_document: payload.data === undefined ? []:payload.data,
-            }
-        },
-        deleteTech_document(state, {payload}) {
-            return{
-                ...state,
-                tech_document: payload.data === undefined ? tech_document:payload.data,
-            }
-        }
-    }
-}
+  reducers: {
+    clear() {
+      return {
+        loading: fasle,
+        tech_document: [],
+      };
+    },
+    changeTech_document(state, { payload }) {
+      return {
+        ...state,
+        tech_document: payload.data === undefined ? [] : payload.data,
+      };
+    },
+    deleteTech_document(state, { payload }) {
+      return {
+        ...state,
+        tech_document: payload.data === undefined ? tech_document : payload.data,
+      };
+    },
+  },
+};
