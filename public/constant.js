98f5f2737b89901d8ddc1a1873089fcfa6988460
caizj15 <caizj15@qq.com>
2018-10-13 02:07:04 +0800

    change public/constant

--- public/constant.js
+++ public/constant.js
@@ -1,4 +1,18 @@
 export const httpAddress = 'http://39.104.208.4';
+
+export const source_data = [
+    { id: 1, title: '文件操作-管理' },
+    { id: 2, title: '文件绑定-管理' },
+    { id: 3, title: '文件可视性-管理' },
+    { id: 4, title: '窗口设计-管理' },
+    { id: 5, title: '网站公告-管理' },
+    { id: 6, title: '未确定-管理' },
+    { id: 7, title: '未确定-管理' },
+    { id: 8, title: '未确定-管理' },
+    { id: 9, title: '未确定-管理' },
+  ];
+
 export default {
     httpAddress,
+    source_data,
 }
