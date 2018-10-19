52fb9005ee36936bdbbb1eda9e79ba146a2e61af
caizj15 <caizj15@qq.com>
2018-10-19 11:35:33 +0800

    change proxy_to_webpackrc

--- .webpackrc.js
+++ .webpackrc.js
@@ -14,6 +14,96 @@ export default {
   },
   //
   proxy: {
+    '/api/v1/user/login': {
+      target: 'http://127.0.0.1:8080/dmcs',
+      changeOrigin: true,
+      pathRewrite: { '^api/v1/user/login': '' },
+    },
+    '/api/v1/user/temcheck': {
+      target: 'http://127.0.0.1:8080/dmcs',
+      changeOrigin: true,
+      pathRewrite: { '^api/v1/user/temcheck': '' },
+    },
+    '/api/v1/user/register': {
+      target: 'http://127.0.0.1:8080/dmcs',
+      changeOrigin: true,
+      pathRewrite: { '^api/v1/user/register': '' },
+    },
+    '/api/v1/user/getuser': {
+      target: 'http://127.0.0.1:8080/dmcs',
+      changeOrigin: true,
+      pathRewrite: { '^api/v1/user/getuser': '' },
+    },
+    '/api/v1/user/logout': {
+      target: 'http://127.0.0.1:8080/dmcs',
+      changeOrigin: true,
+      pathRewrite: { '^api/v1/user/logout': '' },
+    },
+    '/api/v1/user/verifyaccount': {
+      target: 'http://127.0.0.1:8080/dmcs',
+      changeOrigin: true,
+      pathRewrite: { '^api/v1/user/verifyaccount': '' },
+    },
+    '/api/v1/user/motivate': {
+      target: 'http://127.0.0.1:8080/dmcs',
+      changeOrigin: true,
+      pathRewrite: { '^api/v1/user/motivate': '' },
+    },
+    '/api/v1/user/updateuser': {
+      target: 'http://127.0.0.1:8080/dmcs',
+      changeOrigin: true,
+      pathRewrite: { '^api/v1/user/updateuser': '' },
+    },
+    '/api/v1/admin/getAdminuser': {
+      target: 'http://127.0.0.1:8080/dmcs',
+      changeOrigin: true,
+      pathRewrite: { '^api/v1/admin/getAdminuser': '' },
+    },
+    '/api/v1/admin/addAdminuser': {
+      target: 'http://127.0.0.1:8080/dmcs',
+      changeOrigin: true,
+      pathRewrite: { '^api/v1/admin/addAdminuser': '' },
+    },
+    '/api/v1/admin/changeAuthority': {
+      target: 'http://127.0.0.1:8080/dmcs',
+      changeOrigin: true,
+      pathRewrite: { '^api/v1/admin/changeAuthority': '' },
+    },
+    '/api/v1/admin/getSelfuser': {
+      target: 'http://127.0.0.1:8080/dmcs',
+      changeOrigin: true,
+      pathRewrite: { '^api/v1/admin/getSelfuser': '' },
+    },
+    '/api/v1/admin/getdocument': {
+      target: 'http://127.0.0.1:8080/dmcs',
+      changeOrigin: true,
+      pathRewrite: { '^api/v1/admin/getdocument': '' },
+    },
+    '/api/v1/admin/deletedocument': {
+      target: 'http://127.0.0.1:8080/dmcs',
+      changeOrigin: true,
+      pathRewrite: { '^api/v1/admin/deletedocument': '' },
+    },
+    '/api/v1/file/deleteFile': {
+      target: 'http://127.0.0.1:8080/dmcs',
+      changeOrigin: true,
+      pathRewrite: { '^api/v1/file/deleteFile': '' },
+    },
+    '/api/v1/file/getFilelist': {
+      target: 'http://127.0.0.1:8080/dmcs',
+      changeOrigin: true,
+      pathRewrite: { '^api/v1/file/getFilelist': '' },
+    },
+    '/api/v1/file/getImagelist': {
+      target: 'http://127.0.0.1:8080/dmcs',
+      changeOrigin: true,
+      pathRewrite: { '^api/v1/file/getImagelist': '' },
+    },
+    '/api/v1/file/getFileToken': {
+      target: 'http://127.0.0.1:8080/dmcs',
+      changeOrigin: true,
+      pathRewrite: { '^api/v1/file/getFileToken': '' },
+    },
     '/api/v1/user/image': {
       target: 'http://127.0.0.1:8080/dmcs',
       changeOrigin: true,
