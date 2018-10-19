8ec1a686d2c09b5707c2e5e7340224b227555f20
caizj15 <caizj15@qq.com>
2018-10-19 11:34:52 +0800

    change proxt_roadhogrc_to .webpack

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -75,8 +75,6 @@ const proxy = {
   /*'POST /api/v1/user/register': (req, res) => {
     res.send({ status: 'ok', currentAuthority: 'user' });
   }, */
-
-  'POST /api/v1/user/login': 'http://127.0.0.1:8080/dmcs',
   'POST /api/v1/user/temcheck': 'http://127.0.0.1:8080/dmcs',
   'POST /api/v1/user/register': 'http://127.0.0.1:8080/dmcs',
   'POST /api/v1/user/getuser': 'http://127.0.0.1:8080/dmcs',
