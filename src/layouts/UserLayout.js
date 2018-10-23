4c32bfd299654ad1f2d5a38ce184e2f1465a0dff
caizj15 <caizj15@qq.com>
2018-10-23 20:55:43 +0800

    change UserLayout.js

--- src/layouts/UserLayout.js
+++ src/layouts/UserLayout.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Link, Route } from 'dva/router';
+import { connect } from 'dva';
+import { Route } from 'dva/router';
 import DocumentTitle from 'react-document-title';
 import { Icon, Layout } from 'antd';
 import GlobalFooter from '../components/GlobalFooter';
 import styles from './UserLayout.less';
-import logo from '../assets/logo.svg';
 import { getRoutes } from '../utils/utils';
+import { cookieToJson } from '../utils/cookieToJson';
 
-const {Content} = Layout;
-const links = [{
+const { Content } = Layout;
+
+/* const links = [{
   title: '帮助',
   href: '',
 }, {
@@ -17,11 +19,23 @@ const links = [{
 }, {
   title: '条款',
   href: '',
-}];
+}]; */
 
-const copyright = <div>Copyright <Icon type="copyright" /> 2018 清华大学出品</div>;
+// const copyright = <div>Copyright <Icon type="copyright" /> 2018 清华大学出品</div>;
 
 class UserLayout extends React.PureComponent {
+  componentDidMount() {
+    const { currentUser, dispatch } = this.props;
+    const cookie = cookieToJson();
+    if (JSON.stringify(cookie) !== '{}') {
+      if (currentUser.username === undefined || currentUser.username === null) {
+        dispatch({
+          type: 'login/temcheck',
+        });
+      }
+    }
+  }
+
   getPageTitle() {
     const { routerData, location } = this.props;
     const { pathname } = location;
@@ -31,54 +45,56 @@ class UserLayout extends React.PureComponent {
     }
     return title;
   }
+
   render() {
     const { routerData, match } = this.props;
     return (
       <DocumentTitle title={this.getPageTitle()}>
         <Layout className={styles.container}>
-        
-          <Content /*style={{ margin: '24px 24px 0', height: '100%' }}*/>
-          <div style={{ minHeight: 'calc(100vh - 260px)' }}>
-            {
-              getRoutes(match.path, routerData).map(item =>
-                (
-                  <Route
-                    key={item.key}
-                    path={item.path}
-                    component={item.component}
-                    exact={item.exact}
-                  />
-                )
-              )
-            }
-          </div>
-          
+          <Content>
+            <div style={{ minHeight: 'calc(100vh - 260px)' }}>
+              {getRoutes(match.path, routerData).map(item => (
+                <Route
+                  key={item.key}
+                  path={item.path}
+                  component={item.component}
+                  exact={item.exact}
+                />
+              ))}
+            </div>
+
             <GlobalFooter
-              links={[{
-                title: '清华大学首页',
-                href: 'http://www.tsinghua.edu.cn/',
-                blankTarget: true,
-              }, {
-                title: 'GitHub',
-                href: 'https://github.com/chenning007/dmcs-h5',
-                blankTarget: true,
-              }, {
-                title: '清华大学精密仪器系',
-                href: 'http://www.tsinghua.edu.cn/publish/dpi/',
-                blankTarget: true,
-              }]}
+              links={[
+                {
+                  title: '清华大学首页',
+                  href: 'http://www.tsinghua.edu.cn/',
+                  blankTarget: true,
+                },
+                {
+                  title: 'GitHub',
+                  href: 'https://github.com/chenning007/dmcs-h5',
+                  blankTarget: true,
+                },
+                {
+                  title: '清华大学精密仪器系',
+                  href: 'http://www.tsinghua.edu.cn/publish/dpi/',
+                  blankTarget: true,
+                },
+              ]}
               copyright={
                 <div>
                   Copyright <Icon type="copyright" /> 2018 清华大学出品
                 </div>
               }
             />
-          
           </Content>
         </Layout>
       </DocumentTitle>
     );
   }
 }
-//className={styles.footer}
-export default UserLayout;
+// className={styles.footer}
+
+export default connect(state => ({
+  currentUser: state.login.currentUser,
+}))(UserLayout);
