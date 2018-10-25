7909e9302f3c749544d1e03af5677f7670f9825d
caizj15 <caizj15@qq.com>
2018-10-25 21:08:34 +0800

    change Device

--- src/routes/Dashboard/Device.js
+++ src/routes/Dashboard/Device.js
@@ -1,76 +1,70 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
 import { routerRedux } from 'dva/router';
-import { Row, Col, Card,  Avatar, } from 'antd';
+import { Row, Col, Card, Avatar } from 'antd';
 import { getAuthority } from '../../utils/authority';
 
 import styles from './Device.less';
 
-
 @connect(state => ({
-  myself_device: state.device.myself_device,
+  myselfDevice: state.device.myselfDevice,
   loading: state.device.loading,
-  currentUser: state.user.currentUser, 
+  currentUser: state.user.currentUser,
 }))
 export default class Device extends PureComponent {
-  state = {
-    selectedRows: [],
-    formValues: {},
-  };
   componentDidMount() {
     const { dispatch, currentUser } = this.props;
-    if((currentUser!==undefined)&&(currentUser!=={})){
-        dispatch ({
-          type: 'device/fetch',
-          payload: {
-            count: currentUser.userNumber,
-          },
-        }
-        );
+    if (currentUser !== undefined && currentUser !== {}) {
+      dispatch({
+        type: 'device/fetch',
+        payload: {
+          count: currentUser.userNumber,
+        },
+      });
     }
   }
 
   componentWillUnmount() {
     const { dispatch } = this.props;
-    dispatch ({
+    dispatch({
       type: 'device/clear',
-    })
+    });
   }
-/**** */
-  onLinktodevice = (avatar_src, title_src, deviceNumber_src) => {
-    const {dispatch}=this.props;
-    dispatch(routerRedux.push({
-      pathname: '/dashboard/monitor_dgo/monitor_device',
-      state: { 
-        avatar: avatar_src,
-        title: title_src,
-        deviceNumber: deviceNumber_src,
-      },
-    })
+
+  /* *** */
+
+  onLinktodevice = (avatarSrc, titleSrc, deviceNumberSrc) => {
+    const { dispatch } = this.props;
+    dispatch(
+      routerRedux.push({
+        pathname: '/dashboard/monitor_dgo/monitor_device',
+        state: {
+          avatar: avatarSrc,
+          title: titleSrc,
+          deviceNumber: deviceNumberSrc,
+        },
+      })
     );
-  }
+  };
 
-
- /**** */ 
+  /* *** */
 
   render() {
-    const {
-      myself_device, loading,
-    } = this.props;
+    const { myselfDevice, loading } = this.props;
 
-    let authority = getAuthority();
+    const authority = getAuthority();
     return (
       <div>
-        { authority!=='user' && authority!=='admin' && authority!=='host'
-          &&
-          <Card>
-            邮箱未激活，功能暂时无法使用。
-            如需激活或重新设置邮箱，请到“个人管理”进行激活邮件重发送或邮箱设置
-          </Card>
-        }
-        { (authority ==='user' || authority === 'admin' || authority ==='host')
-          &&
-          (<Row gutter={24}>
+        {authority !== 'user' &&
+          authority !== 'admin' &&
+          authority !== 'host' && (
+            <Card>
+              邮箱未激活，功能暂时无法使用。
+              如需激活或重新设置邮箱，请到“个人管理”进行激活邮件重发送或邮箱设置
+            </Card>
+          )}
+        {(authority === 'user' || authority === 'admin' || authority === 'host') && (
+          <Row gutter={24}>
             <Col xl={24} lg={24} md={24} sm={24} xs={24}>
               <Card
                 className={styles.projectList}
@@ -80,29 +74,31 @@ export default class Device extends PureComponent {
                 loading={loading}
                 bodyStyle={{ padding: 0 }}
               >
-                {  myself_device !== []
-                    &&
-                    myself_device.map(item => (
-                      <Card.Grid className={styles.projectGrid} key={item.key}>
-                        <Card 
-                          bodyStyle={{ textAlign: 'center', padding: 0}} 
-                          bordered={false}
-                          onClick={() =>this.onLinktodevice(item.avatar, item.title, item.deviceNumber)}
-                        >
-                          <Card.Meta
-                              style={{padding: 0}}
-                              title={(
-                              <div className={styles.cardTitle}>
-                                  <Avatar size="large" src={item.avatar}/>
-                              </div>
-                              )}
-                              // description={item.title}
-                          />
-                          <div style={{paddingTop:10}}><b>{item.title}</b></div>
-                        </Card> 
-                      </Card.Grid>
-                    ))
-                  }
+                {myselfDevice !== [] &&
+                  myselfDevice.map(item => (
+                    <Card.Grid className={styles.projectGrid} key={item.key}>
+                      <Card
+                        bodyStyle={{ textAlign: 'center', padding: 0 }}
+                        bordered={false}
+                        onClick={() =>
+                          this.onLinktodevice(item.avatar, item.title, item.deviceNumber)
+                        }
+                      >
+                        <Card.Meta
+                          style={{ padding: 0 }}
+                          title={
+                            <div className={styles.cardTitle}>
+                              <Avatar size="large" src={item.avatar} />
+                            </div>
+                          }
+                          // description={item.title}
+                        />
+                        <div style={{ paddingTop: 10 }}>
+                          <b>{item.title}</b>
+                        </div>
+                      </Card>
+                    </Card.Grid>
+                  ))}
               </Card>
               <Card
                 bodyStyle={{ padding: 0 }}
@@ -112,8 +108,8 @@ export default class Device extends PureComponent {
                 loading={loading}
               />
             </Col>
-          </Row>)
-        }
+          </Row>
+        )}
       </div>
     );
   }
