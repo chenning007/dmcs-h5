c733965df77143b019ffe9f8259818f06537fdde
caizj15 <caizj15@qq.com>
2018-10-26 19:31:45 +0800

    change ManageFiel

--- src/routes/Authority/ManageFile.js
+++ src/routes/Authority/ManageFile.js
@@ -1,62 +1,18 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
-import { Card, Upload, message, Icon, Form, Button, Input } from 'antd';
+import { Card, Upload, message, Icon, Form, Button, Input, Menu } from 'antd';
 import { routerRedux } from 'dva/router';
 import reqwest from 'reqwest';
 import { getAuthority } from '../../utils/authority';
 import { cookieToJson } from '../../utils/cookieToJson';
-/* const source_title = [
-  { id: 1, title: 'DMCS简介-管理' },
-  { id: 2, title: '解决方案-管理' },
-  { id: 3, title: '科研成果-管理' },
-  { id: 4, title: '设计案例-管理' },
-  { id: 5, title: '合作方式-管理' },
-  { id: 6, title: '软件下载-管理' },
-  { id: 7, title: '资料下载-管理' },
-  { id: 8, title: '合作规则-管理' },
-  { id: 9, title: '合作留言-管理' },
-]; */
-
-/* const source_data = [
-  {
-    id: 1,
-    image_address: 'http://39.104.208.4/image/firstpage/shili1.png',
-    description: 'IPT70135/6/7测控节点电路封装盒',
-    document_address: '',
-    title: '',
-  },
-  {
-    id: 2,
-    image_address: 'http://39.104.208.4/image/firstpage/shili2.png',
-    description: 'IPT10533/10550 3.3V/5.0V非隔离式电源转换',
-    document_address: '',
-    title: '',
-  },
-  {
-    id: 3,
-    image_address: 'http://39.104.208.4/image/firstpage/shili3.png',
-    description: 'IPT12105 IPCAN总线供电转换模块',
-    document_address: '',
-    title: '',
-  },
-  {
-    id: 4,
-    image_address: 'http://39.104.208.4/image/firstpage/shili4.png',
-    description: 'IPT22105/22106 IPCAN电源转换节点',
-    document_address: '',
-    title: '',
-  },
-  {
-    id: 5,
-    image_address: 'http://39.104.208.4/image/firstpage/shili5.png',
-    description: 'IPT70540 PCB表贴植锡丝印台',
-    document_address: '',
-    title: '',
-  },
-]; */
 
 @connect(state => ({
-  document: state.document,
+  loding: state.document.loading,
+  fileloding: state.document.fileloading,
+  imageloading: state.document.imageloading,
+  files: state.document.files,
+  images: state.document.images,
+  fileImages: state.document.fileImages,
 }))
 @Form.create()
 export default class ManageFile extends PureComponent {
@@ -66,6 +22,8 @@ export default class ManageFile extends PureComponent {
     uploading: false,
     buAble1: false, // 是否禁止上传图片
     buAble2: false, // 是否禁止上传文件
+    contentType: 'file',
+    showType: 'nofile',
   };
 
   componentWillMount() {
@@ -92,30 +50,35 @@ export default class ManageFile extends PureComponent {
     });
   }
 
-  /* componentWillReceiveProps(nextProps) {
-    if ( (nextProps.document.tech_document.length > this.state.documents.length) ) {
-      this.setState({ documents: [...nextProps.document.tech_document] });
-      this.setState({loading: false});
-    }
-    if( (nextProps.document.tech_document.length < this.state.documents.length)&&(this.state.add_delete)) {
-      this.setState({ documents: [...nextProps.document.tech_document] });
-      this.setState({loading: false, add_delete:false,});
-    }
-  } */
-
-  /* onChangefile(key) {
-    const { dispatch, tem_id } = this.props;
-    let cookie = cookieToJson();
-    dispatch({
-      type: 'document/deleteDocument',
-      payload: {
-        cookie,
-        authority: tem_id,
-        documentId: key,
-      },
+  handleMenuClick = e => {
+    const key = e.key;
+    this.setState({
+      contentType: key,
     });
-    this.setState({add_delete:true, loading: true});
-  } */
+  };
+
+  handleShowClick = e => {
+    const key = e.key;
+    const { dispatch } = this.props;
+    this.setState({
+      showType: key,
+    });
+    if (key === 'file') {
+      dispatch({
+        type: 'document/getFilelist',
+      });
+    }
+    if (key === 'image') {
+      dispatch({
+        type: 'document/getImagelist',
+      });
+    }
+    if (key === 'fileimage') {
+      dispatch({
+        type: 'document/getCommonFileImage',
+      });
+    }
+  };
 
   handleUpload = type => {
     const { filesList, imagesList } = this.state;
@@ -197,12 +160,26 @@ export default class ManageFile extends PureComponent {
     const { dispatch } = this.props;
     dispatch(routerRedux.push('/authority/manage_list'));
   }
-  /**
-   *
-   */
 
   render() {
-    const { imagesList, filesList, buAble2, buAble1 } = this.state;
+    const { imagesList, filesList, buAble2, buAble1, contentType } = this.state;
+
+    const menu = (
+      <Menu onClick={this.handleMenuClick} mode="horizontal" defaultSelectedKeys={['file']}>
+        <Menu.Item key="file">文件上传</Menu.Item>
+        <Menu.Item key="image">图片上传</Menu.Item>
+        <Menu.Item key="fileImage">文件与图片绑定</Menu.Item>
+      </Menu>
+    );
+
+    const showMenu = (
+      <Menu onClick={this.handleShowClick} mode="horizontal">
+        <Menu.Item key="file">文件显示</Menu.Item>
+        <Menu.Item key="image">图片显示</Menu.Item>
+        <Menu.Item key="fileImage">文件图片显示</Menu.Item>
+      </Menu>
+    );
+
     const props1 = {
       action: '/api/v1/file/addFile',
       accept: 'image/*',
@@ -251,121 +228,120 @@ export default class ManageFile extends PureComponent {
     };
     const { form } = this.props;
     const { getFieldDecorator } = form;
-    const { uploading } = this.state;
+    const { uploading, showType } = this.state;
     return (
       <div>
         <Card
-          title="文件"
+          title={menu}
           extra={
             <Button type="primary" onClick={() => this.ReturnRouter()}>
               <Icon type="rollback" />
             </Button>
           }
         >
-          <Upload {...props2}>
-            <Button disabled={buAble2 || buAble1}>
-              <Icon type="upload" />
-              上传文件
-            </Button>
-          </Upload>
-          <Form>
-            <Form.Item
-              colon={false}
-              labelCol={{ span: 4, offset: 0 }}
-              wrapperCol={{ span: 8, offset: 2 }}
-              label={<b>标题:</b>}
-            >
-              {getFieldDecorator('filename', {
-                rules: [
-                  {
-                    required: true,
-                    message: '输入文档标题',
-                  },
-                ],
-              })(<Input placeholder="输入文档标题" />)}
-            </Form.Item>
-            <Form.Item
-              colon={false}
-              labelCol={{ span: 4, offset: 0 }}
-              wrapperCol={{ span: 8, offset: 2 }}
-              label={<b>描述:</b>}
-            >
-              {getFieldDecorator('filedescription', {
-                rules: [
-                  {
-                    required: true,
-                    message: '输入描述内容',
-                  },
-                ],
-              })(<Input placeholder="简要描述文档内容" />)}
-            </Form.Item>
-          </Form>
-          <Button
-            type="primary"
-            onClick={() => this.handleUpload('file')}
-            disabled={filesList.length === 0}
-            loading={uploading}
-          >
-            {uploading ? '上传' : '开始上传'}
-          </Button>
+          {contentType === 'file' && (
+            <div>
+              <Upload {...props2}>
+                <Button disabled={buAble2 || buAble1}>
+                  <Icon type="upload" />
+                  上传文件
+                </Button>
+              </Upload>
+              <Form>
+                <Form.Item
+                  colon={false}
+                  labelCol={{ span: 4, offset: 0 }}
+                  wrapperCol={{ span: 8, offset: 2 }}
+                  label={<b>标题:</b>}
+                >
+                  {getFieldDecorator('filename', {
+                    rules: [
+                      {
+                        required: true,
+                        message: '输入文档标题',
+                      },
+                    ],
+                  })(<Input placeholder="输入文档标题" />)}
+                </Form.Item>
+                <Form.Item
+                  colon={false}
+                  labelCol={{ span: 4, offset: 0 }}
+                  wrapperCol={{ span: 8, offset: 2 }}
+                  label={<b>描述:</b>}
+                >
+                  {getFieldDecorator('filedescription', {
+                    rules: [
+                      {
+                        required: true,
+                        message: '输入描述内容',
+                      },
+                    ],
+                  })(<Input placeholder="简要描述文档内容" />)}
+                </Form.Item>
+              </Form>
+              <Button
+                type="primary"
+                onClick={() => this.handleUpload('file')}
+                disabled={filesList.length === 0}
+                loading={uploading}
+              >
+                {uploading ? '上传' : '开始上传'}
+              </Button>
+            </div>
+          )}
+          {contentType === 'image' && (
+            <div>
+              <Upload {...props1}>
+                <Button disabled={buAble2 || buAble1}>
+                  <Icon type="upload" />
+                  上传图片
+                </Button>
+              </Upload>
+              <Form>
+                <Form.Item
+                  colon={false}
+                  labelCol={{ span: 4, offset: 0 }}
+                  wrapperCol={{ span: 8, offset: 2 }}
+                  label={<b>标题:</b>}
+                >
+                  {getFieldDecorator('imagename', {
+                    rules: [
+                      {
+                        required: true,
+                        message: '输入图片标题',
+                      },
+                    ],
+                  })(<Input placeholder="输入图片标题" />)}
+                </Form.Item>
+                <Form.Item
+                  colon={false}
+                  labelCol={{ span: 4, offset: 0 }}
+                  wrapperCol={{ span: 8, offset: 2 }}
+                  label={<b>描述:</b>}
+                >
+                  {getFieldDecorator('imagedescription', {
+                    rules: [
+                      {
+                        required: true,
+                        message: '输入图片描述内容',
+                      },
+                    ],
+                  })(<Input placeholder="简要描述图片内容" />)}
+                </Form.Item>
+                <Button
+                  type="primary"
+                  onClick={() => this.handleUpload('image')}
+                  disabled={imagesList.length === 0}
+                  loading={uploading}
+                >
+                  {uploading ? '上传' : '开始上传'}
+                </Button>
+              </Form>
+            </div>
+          )}
         </Card>
-        <Card style={{ marginTop: 12 }} title="图片">
-          <Upload {...props1}>
-            <Button disabled={buAble2 || buAble1}>
-              <Icon type="upload" />
-              上传图片
-            </Button>
-          </Upload>
-          <Form>
-            <Form.Item
-              colon={false}
-              labelCol={{ span: 4, offset: 0 }}
-              wrapperCol={{ span: 8, offset: 2 }}
-              label={<b>标题:</b>}
-            >
-              {getFieldDecorator('imagename', {
-                rules: [
-                  {
-                    required: true,
-                    message: '输入图片标题',
-                  },
-                ],
-              })(<Input placeholder="输入图片标题" />)}
-            </Form.Item>
-            <Form.Item
-              colon={false}
-              labelCol={{ span: 4, offset: 0 }}
-              wrapperCol={{ span: 8, offset: 2 }}
-              label={<b>描述:</b>}
-            >
-              {getFieldDecorator('imagedescription', {
-                rules: [
-                  {
-                    required: true,
-                    message: '输入图片描述内容',
-                  },
-                ],
-              })(<Input placeholder="简要描述图片内容" />)}
-            </Form.Item>
-            <Button
-              type="primary"
-              onClick={() => this.handleUpload('image')}
-              disabled={imagesList.length === 0}
-              loading={uploading}
-            >
-              {uploading ? '上传' : '开始上传'}
-            </Button>
-          </Form>
-        </Card>
-        <Card style={{ marginTop: 12, textAlign: 'center' }}>
-          <Button type="primary" block onClick={() => this.ToFileImage('file')}>
-            查看文件
-          </Button>
-        </Card>
-        <Card style={{ marginTop: 12, textAlign: 'center' }}>
-          <Button type="primary" block onClick={() => this.ToFileImage('image')}>
-            查看图片
-          </Button>
+        <Card style={{ marginTop: 12 }} title={showMenu}>
+          {showType === 'nofile' && <span>选择显示的内容</span>}
         </Card>
       </div>
     );
