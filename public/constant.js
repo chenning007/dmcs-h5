f3f18493537a07b0628960de5055443b422fe1fe
caizj15 <caizj15@qq.com>
2018-10-28 00:43:46 +0800

    change constant

--- public/constant.js
+++ public/constant.js
@@ -1,12 +1,12 @@
-export const httpAddress = /* 'http://39.104.208.4 ;'; */ 'http://127.0.0.1:80';
+export const httpAddress = 'http://39.104.208.4'; /* 'http://127.0.0.1:80'; */
 
 export const sourceData = [
-  { id: 10, title: '权限维护', icon: 'lock', color: { backgroundColor: '#008000' } },
+  { id: 10, title: '权限管理', icon: 'lock', color: { backgroundColor: '#008000' } },
   { id: 1, title: '文件操作', icon: 'file', color: { backgroundColor: '#008000' } },
   { id: 2, title: '窗口绑定', icon: 'select', color: { backgroundColor: '#008000' } },
   { id: 3, title: '显示管理', icon: 'appstore', color: { backgroundColor: '#008000' } },
   { id: 4, title: '栏目维护', icon: 'layout', color: { backgroundColor: '#008000' } },
-  { id: 5, title: '公告内容', icon: 'profile', color: { backgroundColor: '#008000' } },
+  { id: 5, title: '网站公告', icon: 'profile', color: { backgroundColor: '#008000' } },
   { id: 6, title: '待定', icon: 'question-circle', color: { backgroundColor: '#ffff00' } },
   { id: 7, title: '待定', icon: 'question-circle', color: { backgroundColor: '#ffff00' } },
   { id: 8, title: '待定', icon: 'question-circle', color: { backgroundColor: '#ffff00' } },
