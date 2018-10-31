8cbf61671824b5e0b2ab6d5e2005bb2e2565281e
caizj15 <caizj15@qq.com>
2018-10-31 23:06:18 +0800

    change BasicLayout

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -17,6 +17,7 @@ import logo from '../assets/image1.png';
 
 import Authorized from '../utils/Authorized';
 import { cookieToJson } from '../utils/cookieToJson';
+import { clearAllCookie } from '../utils/clearCookie';
 
 const { AuthorizedRoute } = Authorized;
 
@@ -60,15 +61,6 @@ const query = {
     minWidth: 1200,
   },
 };
-
-// 清除所有cookie函数
-function clearAllCookie() {
-  const keys = document.cookie.match(/[^ =;]+(?=)/g);
-  if (keys) {
-    for (let i = keys.length; (i -= 1); )
-      document.cookie = `${keys[i]}=0;expires=${new Date(0).toUTCString()}`;
-  }
-}
 
 class BasicLayout extends React.PureComponent {
   static childContextTypes = {
