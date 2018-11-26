3b4019fd2204c8858acdfd253dbd0a3f1698fca0
caizj15 <caizj15@qq.com>
2018-11-26 13:36:52 +0800

    change KeyToName

---
+++ src/utils/KeyToName.js
@@ -0,0 +1,47 @@
+export function KeytoName(key) {
+  switch (key) {
+    case '1':
+      return '首页';
+    case '2':
+      return 'DMCS简介';
+    case '3':
+      return '解决方案';
+    case '4':
+      return '科研成果';
+    case '5':
+      return '设计案例';
+    case '6':
+      return '合作方式';
+    case '7':
+      return '软件下载';
+    case '8':
+      return '资料下载';
+    case '9':
+      return '合作规则';
+    default:
+      return '首页';
+  }
+}
+
+export function KeytoModule(key) {
+  switch (key) {
+    case '2':
+      return 'b';
+    case '3':
+      return 'c';
+    case '4':
+      return 'd';
+    case '5':
+      return 'e';
+    case '6':
+      return 'f';
+    case '7':
+      return 'g';
+    case '8':
+      return 'h';
+    case '9':
+      return 'i';
+    default:
+      return 'a';
+  }
+}
